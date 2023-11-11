55ac244cc9a315fd67508c98c522807931f8a152
Clement127001 <babiyonclement18@gmail.com>
2023-11-11 12:59:26 +0530

    add the initial router setup

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+const authrouter = require("./routes/auth");
+const jobRouter = require("./routes/jobs");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use("/api/v1/auth", authrouter);
+app.use("/api/v1/jobs", jobRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
